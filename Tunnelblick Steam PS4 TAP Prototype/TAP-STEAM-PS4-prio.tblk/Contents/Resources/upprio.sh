908589125316b8d53817dbe08761cf8dc682a53b
labor4 <schreibtisch@labor4.ch>
2019-04-29 22:45:49 +0200

    Update upprio.sh

--- Tunnelblick Steam PS4 TAP Prototype/TAP-STEAM-PS4-prio.tblk/Contents/Resources/upprio.sh
+++ Tunnelblick Steam PS4 TAP Prototype/TAP-STEAM-PS4-prio.tblk/Contents/Resources/upprio.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Basically this sets the tap net at higher Priority than the hw device
-# Which makes it usable for PS4 Remote Play as well.
 # MYGAMEROUTERIP is the gateway of the net in which Steam server and PS4 reside.
+# $trusted_ip is an ovpn env var
 
 MYGAMEROUTERIP=10.24.60.1
 mydef=$(/sbin/route -n get default | grep 'gateway' | awk '{print $2}')
